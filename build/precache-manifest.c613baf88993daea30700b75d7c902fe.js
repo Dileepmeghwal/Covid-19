eb8630ef7f3da2ccdbfda4c195b00e4a61f3f8de
Dileep Kumar Meghwal <58825727+Dileepmeghwal@users.noreply.github.com>
2021-02-10 22:53:48 +0530

    covid-19

---
+++ build/precache-manifest.c613baf88993daea30700b75d7c902fe.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2e8c66f82e0711d586fd59a462b176d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "35948edbc8ea1cb7f94f",
+    "url": "/static/css/main.588b7d1d.chunk.css"
+  },
+  {
+    "revision": "a318148e416e3d61abd9",
+    "url": "/static/js/2.f33dfd24.chunk.js"
+  },
+  {
+    "revision": "1ef8d540b3072f991ff256dfc61c7bcd",
+    "url": "/static/js/2.f33dfd24.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35948edbc8ea1cb7f94f",
+    "url": "/static/js/main.157450f5.chunk.js"
+  },
+  {
+    "revision": "d2413320a277bb2c659c",
+    "url": "/static/js/runtime-main.c14436a3.js"
+  },
+  {
+    "revision": "a52da43d4ad436b0c3ef87281865aeeb",
+    "url": "/static/media/covid-19.a52da43d.png"
+  }
+]);
